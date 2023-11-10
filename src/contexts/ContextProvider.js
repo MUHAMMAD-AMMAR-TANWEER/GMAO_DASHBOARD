6fd6a51605c8c50f453a0bb7bb90c025d0ef9c69
MUHAMMAD-AMMAR-TANWEER <ammartanweer7370@gmail.com>
2023-11-10 03:12:07 -0800

    proper context

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -230,7 +230,7 @@ export const ContextProvider = ({ children }) => {
     });
     setMaxValue1(max_y + 2);
     console.log("Maximum value of y:", max_y);
-    setLine([response.data.Line1[0], response.data.Line1[1]]);
+    setLine([response.data.Line1[0], response.data.Line1[1]])
 
     // LINE 2
     let max_y2 = Number.MIN_SAFE_INTEGER;
@@ -243,31 +243,31 @@ export const ContextProvider = ({ children }) => {
     });
     setMaxValue2(max_y2 + 2);
     console.log("Maximum value of y:", max_y2);
-    setLine2([response.data.Line2[0], response.data.Line2[1]]);
+    setLine2([response.data.Line2[0], response.data.Line2[1]])
 
     // Bar 1
     setAvg1(parseFloat(response.data.Bar1.avg).toFixed(2));
-    setBar1(response.data.Bar1.values[0]);
+    setBar1(response.data.Bar1.values[0])
 
     // Bar 2
     setAvg2(parseFloat(response.data.Bar2.avg).toFixed(2));
-    setBar2(response.data.Bar2.values[0]);
+    setBar2(response.data.Bar2.values[0])
 
     // Bar 3
     setAvg3(parseFloat(response.data.Bar3.avg).toFixed(2));
-    setBar3(response.data.Bar3.values[0]);
+    setBar3(response.data.Bar3.values[0])
 
     // Bar 4
     setAvg4(parseFloat(response.data.Bar4.avg).toFixed(2));
-    setBar4(response.data.Bar4.values[0]);
+    setBar4(response.data.Bar4.values[0])
 
     // Horizontal 1
     setXYValues(response.data.H1.values);
-    setHorizontalbar1(response.data.H1.values);
+    setHorizontalbar1(response.data.H1.values)
 
     // Horizontal 2
     setXYValues2(response.data.H2.values);
-    setHorizontalbar2(response.data.H2.values);
+    setHorizontalbar2(response.data.H2.values)
 
     // Table 1
         setCompletedData2(response.data.Table1.completed);
@@ -278,44 +278,26 @@ export const ContextProvider = ({ children }) => {
     setmairieData2(response.data.Table1.mairie);
     setMaintenanceZefilData2(response.data.Table1.maintenanceZefil);
 
-    setTable(response.data.Table1)
+    setTable2(response.data.Table1)
 
     //Table 2
 
-            setCompletedData2(response.data.Table2.completed);
-    setRemainingData2(response.data.Table2.remaining);
-    setTotalData2(response.data.Table2.total);
-    setsnefData2(response.data.Table2.snef);
-    setsipproData2(response.data.Table2.sippro);
-    setmairieData2(response.data.Table2.mairie);
-    setMaintenanceZefilData2(response.data.Table2.maintenanceZefil);
+            setCompletedData(response.data.Table2.completed);
+    setRemainingData(response.data.Table2.remaining);
+    setTotalData(response.data.Table2.total);
+    setsnefData(response.data.Table2.snef);
+    setsipproData(response.data.Table2.sippro);
+    setmairieData(response.data.Table2.mairie);
+    setMaintenanceZefilData(response.data.Table2.maintenanceZefil);
 
-    setTable2(response.data.Table2)
+    setTable(response.data.Table2)
   }
   
   useEffect(() => {
     (async () => {
       if (startDate && endDate) {
-        const fetchLineData = await lineData1();
-        setLine(fetchLineData);
-        const fetchLineData2 = await lineData2();
-        setLine2(fetchLineData2);
-        const tableData = await getTableData();
-        setTable(tableData);
-        const tableData2 = await getTableData2();
-        setTable2(tableData2);
-        // const fetchHorizontalData1 = await HorizontalData1();
-        // setHorizontalbar1(fetchHorizontalData1);
-        // const fetchHorizontalData2 = await HorizontalData2();
-        // setHorizontalbar2(fetchHorizontalData2);
-        // const fetchBarData1 = await BarData1();
-        // setBar1(fetchBarData1);
-        // const fetchBarData2 = await BarData2();
-        // setBar2(fetchBarData2);
-        // const fetchBarData3 = await BarData3();
-        // setBar3(fetchBarData3);
-        // const fetchBarData4 = await BarData4();
-        // setBar4(fetchBarData4);
+        const GeneralData = await generalApi();
+
 
         
       }
@@ -383,4 +365,4 @@ export const ContextProvider = ({ children }) => {
   );
 };
 
-export const useStateContext = () => useContext(StateContext);
+export const useStateContext = () => useContext(StateContext)
